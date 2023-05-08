656b77312ed47ebd520cde0629f61d5ad2390b44
DreisteineSCU <dreisteine262@163.com>
2023-05-08 17:41:49 +0800

    add: 加入SHAP计算案例和代码流程梳理

--- script/VTE模型解释/model_interpretation.py
+++ script/VTE模型解释/model_interpretation.py
@@ -14,7 +14,7 @@ Created on 2023/05/07 14:14:26
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
-from shap import Explainer, Explanation
+from shap import TreeExplainer, Explanation
 import shap
 import pandas as pd
 import numpy as np
@@ -33,7 +33,8 @@ if __name__ == "__main__":
     # 数据载入 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ################################################################################################
 
-    data = pd.read_csv("data.csv")
+    data = pd.read_csv(f"{BASE_DIR}/data/data.csv")
+    # data = pd.read_csv("data/data.csv")
     data.drop(["INPATIENT_ID", "SEX", "INJURY_TYPE", "INJURY_CAUSE"], axis=1, inplace=True)
     data = data.dropna(axis=0)
 
@@ -54,19 +55,25 @@ if __name__ == "__main__":
     # 模型解释 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ################################################################################################
     
-    # 创建Explainer示例用于计算SHAP值
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer(X_test)
+    shap_values = explainer(X_test)  # shape=(sample_size, feature, label)
+    y_pred = model.predict_proba(X_test)
     
-    # shap.summary_plot(shap_values, X_test)
+    np.abs(shap_values.values.sum(1) + shap_values.base_values - y_pred).max()
     
-    # 1. beeswarm + bar图
-    plt.figure(figsize=(6, 6))
-    shap.plots.beeswarm(shap_values, max_display=30, show=False)
-    plt.tight_layout()
-    # plt.savefig(f"{BASE_DIR}/case_TTF_prediction/img/beeswarm.png", dpi=450)
+    # # 创建Explainer示例用于计算SHAP值
+    # explainer = shap.TreeExplainer(model)
+    # shap_values = explainer(X_test)
+    
+    # # shap.summary_plot(shap_values, X_test)
+    
+    # # 1. beeswarm + bar图
+    # plt.figure(figsize=(6, 6))
+    # shap.plots.beeswarm(shap_values, max_display=30, show=False)
+    # plt.tight_layout()
+    # # plt.savefig(f"{BASE_DIR}/case_TTF_prediction/img/beeswarm.png", dpi=450)
 
-    plt.figure(figsize=(6, 6))
-    shap.plots.bar(shap_values, max_display=30, show=False)
-    plt.tight_layout()
-    # plt.savefig(f"{BASE_DIR}/case_TTF_prediction/img/bar.png", dpi=450)
+    # plt.figure(figsize=(6, 6))
+    # shap.plots.bar(shap_values, max_display=30, show=False)
+    # plt.tight_layout()
+    # # plt.savefig(f"{BASE_DIR}/case_TTF_prediction/img/bar.png", dpi=450)
