a035d82ce35952909fafa2f0a8946bc86723150d
Dreisteine-Matebook16 <dreisteine262@163.com>
2023-09-15 13:48:44 +0800

    update: 更新文档

--- script/粒子滤波/离散迭代_粒子滤波.py
+++ script/粒子滤波/离散迭代_粒子滤波.py
@@ -1,19 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2023/09/14 11:31:22
-
-@File -> 基于粒子滤波的输入分布估计.py
-
-@Author: luolei
-
-@Email: dreisteine262@163.com
-
-@Describe: 基于粒子滤波的输入分布估计
-"""
-
 from scipy import stats
 import numpy as np
-# import arviz as az
 import sys
 import os
 
@@ -25,7 +11,6 @@ from setting import plt
 
 def cal_sys_output(x):
     return x + 1
-    # return x**2
 
 
 def _get_accum_weights(weights):
@@ -38,20 +23,15 @@ def _get_accum_weights(weights):
     return accum_weights
 
 
-
-if __name__ == "__main__":
+def gen_data(sys_noise_std, obs_noise_std, N):
+    """生成模拟数据"""
+    x_init = 1.1
+    x_obs_init = x_init + np.random.normal(0, obs_noise_std)
     
-    # ---- 产生模拟样本 ------------------------------------------------------------------------------
+    x_series = np.array([x_init])          # 状态值
+    x_obs_series = np.array([x_obs_init])      # 观测值
 
-    # 噪声参数
-    sys_noise_std = 0.1
-    obs_noise_std = 10
-
-    N = 100
-    x_series = np.array([0.1])          # 状态值
-    x_obs_series = np.array([0.1])      # 观测值
-
-    for _ in range(N):
+    for _ in range(N - 1):
         xi = x_series[-1]
         sys_noise = np.random.normal(0, sys_noise_std)
         obs_noise = np.random.normal(0, obs_noise_std)
@@ -60,32 +40,54 @@ if __name__ == "__main__":
 
         x_series = np.append(x_series, yi)
         x_obs_series = np.append(x_obs_series, y_obs)
+        
+    return x_series, x_obs_series
 
-    # plt.plot(np.arange(len(x_series)), x_series, c="k", lw=0.6)
-    plt.figure(figsize=(5, 5))
-    plt.scatter(np.arange(len(x_obs_series)), x_obs_series, marker="o", s=6, c="w", edgecolors="k", lw=0.6)
-    # plt.show()
 
-    # ---- 进行滤波 ---------------------------------------------------------------------------------
+def resample(particles, weights):
+    """重要性采样"""
+    accum_weights = _get_accum_weights(weights)
 
-    n_particles = 1000
-    # x_range = [np.min(x_obs_series), np.max(x_obs_series)]
+    rand_nums = np.random.random(len(weights))
+    particles_resampled = np.array([])
+    for num in rand_nums:
+        probs_sort = np.sort(np.append(accum_weights, num))
+        insert_idx = np.argwhere(probs_sort == num)[0][0]
+        particles_resampled = np.append(particles_resampled, particles[insert_idx])
+    
+    return particles_resampled
+    
 
+if __name__ == "__main__":
+    
+    # ---- 产生模拟样本 ------------------------------------------------------------------------------
+
+    # 噪声参数
     sys_noise_std = 0.1
     obs_noise_std = 10
-    pf_rounds = 10 # PF迭代收敛次数 # TODO: 讨论是否需要迭代?
-
+    
+    N = 100
+    
+    x_series, x_obs_series = gen_data(sys_noise_std, obs_noise_std, N)
+    
+    # ---- 粒子滤波 ---------------------------------------------------------------------------------
+    
+    n_particles = 1000
+    sys_noise_std = 0.1
+    obs_noise_std = 10
+    x_range = [-500, 500]  # NOTE: 需要同时涵盖样本和外推样本
+    
     x_filtered = []
-    for loc in range(N):
+    for loc in range(N - 1):
         print(f"loc = {loc}, \ttotal = {N - 1} \r", end="")
-
-        # 观测值
-        y_obs = x_obs_series[loc + 1]
-        x_range = [y_obs - 50, y_obs + 50]
         
+        # 初始化粒子集
         if loc == 0:
             particles = np.linspace(x_range[0], x_range[1], n_particles)
-
+        
+        # 输出观测值
+        y_obs = x_obs_series[loc + 1]
+        
         # 前向并计算权重
         weights = np.array([])
         for x_sys in particles:
@@ -98,63 +100,51 @@ if __name__ == "__main__":
             # 校正：根据输出与观测的匹配程度，计算各粒子的重要度权重 wi = p(y_obs|xi)
             wi = stats.norm.pdf(y_obs, y_sys, obs_noise_std)
             weights = np.append(weights, wi)
-
+        
         # 重要性采样
-        accum_weights = _get_accum_weights(weights)
-
-        rand_nums = np.random.random(n_particles)
-        particles_resampled = np.array([])
-        for num in rand_nums:
-            probs_sort = np.sort(np.append(accum_weights, num))
-            insert_idx = np.argwhere(probs_sort == num)[0][0]
-            particles_resampled = np.append(particles_resampled, particles[insert_idx])
-
-        x_filtered.append(np.mean(particles))
+        particles_resampled = resample(particles, weights)
+        
+        # 记录结果
+        x_filtered.append(np.mean(particles))  # 本环节输入状态值期望 E[x_sys]
         
         # 更新粒子: 预测下一时刻的N个粒子
+        sys_noise = np.random.normal(0, sys_noise_std, n_particles)
         particles = cal_sys_output(particles_resampled) + sys_noise
+    
+    # 最后一个样本的滤波值
+    x_filtered.append(np.mean(particles))
+    
+    # ---- 进行外推 ---------------------------------------------------------------------------------
+    
+    N_pred = 200
+    x_pred, x_pred_std = [], []
+    for _ in range(N_pred):
+        sys_noise = np.random.normal(0, sys_noise_std, n_particles)
+        particles = cal_sys_output(particles) + sys_noise
         
-        # >>>>>>>>
-        # for _ in range(pf_rounds):
-        #     # 前向并计算权重
-        #     weights = np.array([])
-        #     for x_sys in particles:
-        #         sys_noise = np.random.normal(0, sys_noise_std)
-        #         obs_noise = np.random.normal(0, obs_noise_std)
-
-        #         # 预测：前向计算
-        #         y_sys = cal_sys_output(x_sys) + sys_noise
-
-        #         # 校正：根据输出与观测的匹配程度，计算各粒子的重要度权重 wi = p(y_obs|xi)
-        #         wi = stats.norm.pdf(y_obs, y_sys, obs_noise_std)
-        #         weights = np.append(weights, wi)
-
-        #     # 重要性采样
-        #     accum_weights = _get_accum_weights(weights)
-
-        #     rand_nums = np.random.random(n_particles)
-        #     particles_resampled = np.array([])
-        #     for num in rand_nums:
-        #         probs_sort = np.sort(np.append(accum_weights, num))
-        #         insert_idx = np.argwhere(probs_sort == num)[0][0]
-        #         particles_resampled = np.append(particles_resampled, particles[insert_idx])
-
-        #     # 更新粒子
-        #     particles = particles_resampled
-
-    xx = np.arange(len(x_filtered))
+        x_pred.append(np.mean(particles))  # 本环节输入状态值期望 E[x_sys]
+        x_pred_std.append(np.std(particles))
+    
+    # 真实关系    
+    xx = np.arange(N + N_pred)
     yy = cal_sys_output(xx)
     
+    x_pred, x_pred_std = np.array(x_pred), np.array(x_pred_std)
+    
+    plt.figure(figsize=(5, 5))
+    plt.scatter(np.arange(N), x_obs_series, marker="o", s=6, c="w", edgecolors="k", lw=0.6)
     plt.plot(xx, yy, "--", c="k", linewidth=2)
     plt.plot(x_filtered, linewidth=2)
+    # plt.plot(np.arange(N, N + N_pred), x_pred, linewidth=2, color="r")
+    plt.fill_between(np.arange(N, N + N_pred), x_pred - 3 * x_pred_std, x_pred + 3 * x_pred_std, alpha=0.3)
     plt.xlabel("$t$")
     plt.ylabel("$x$")
-    plt.grid(True, linewidth=0.5)
+    plt.grid(True, linewidth=0.5, alpha=0.5)
     plt.savefig("img/离散迭代滤波结果.png", dpi=450)
     plt.show()
         
+        
     
-    
-    
-    
+
+
     
