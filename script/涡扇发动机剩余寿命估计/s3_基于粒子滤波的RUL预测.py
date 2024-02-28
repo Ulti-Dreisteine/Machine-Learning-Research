aeb5b50c4a46f80284ef4edc8fe6c8fc1ce85a74
Dreisteine-Matebook16 <dreisteine262@163.com>
2024-02-28 16:09:23 +0800

    TODO: 继续粒子滤波

--- script/涡扇发动机剩余寿命估计/s3_基于粒子滤波的RUL预测.py
+++ script/涡扇发动机剩余寿命估计/s3_基于粒子滤波的RUL预测.py
@@ -47,6 +47,10 @@ def get_finallife_x_samples(data: pd.DataFrame, x_col: str) -> np.ndarray:
 
 
 def estimate_params(t_series: np.ndarray, obs_samples: np.ndarray) -> tuple[float, float, float, float]:
+    """
+    对参数进行贝叶斯估计
+    """
+    
     params2est = [
         "a",            # 老化因子
         "b",            # X的初始状态
@@ -91,6 +95,8 @@ def estimate_params(t_series: np.ndarray, obs_samples: np.ndarray) -> tuple[floa
     return float(a_map), float(b_map), float(c_map), float(sigma_obs_map)
 
 
+# 重要性采样
+
 def _get_accum_weights(weights):
     """
     将权重转化为累计值
@@ -105,23 +111,23 @@ def _get_accum_weights(weights):
     return accum_weights
 
 
-def resample(particles, weights):
+def resample(pts, weights):
     """
-    重要性采样
+    重要性采样：根据权重weights对粒子pts进行重采样
     """
     
     accum_weights = _get_accum_weights(weights)
 
     rand_nums = np.random.random(len(weights))
-    particles_resampled = None
+    pts_resampled = None
     for num in rand_nums:
         probs_sort = np.sort(np.append(accum_weights, num))
         insert_idx = np.argwhere(probs_sort == num)[0][0]
         
-        particles_resampled = particles[insert_idx, :] if particles_resampled is None \
-            else np.vstack((particles_resampled, particles[insert_idx, :]))
+        pts_resampled = pts[insert_idx, :] if pts_resampled is None \
+            else np.vstack((pts_resampled, pts[insert_idx, :]))
     
-    return particles_resampled
+    return pts_resampled
 
 
 if __name__ == "__main__":
@@ -146,75 +152,140 @@ if __name__ == "__main__":
     
     # ---- 提取目标设备指标数据 -----------------------------------------------------------------------
     
-    obs_samples = load_unit_test_data(data, unit_nb, x_col)[: 140]
+    obs_samples = load_unit_test_data(data, unit_nb, x_col)[: 140]  # NOTE: 这里只观测前140个样本
     t_series = np.arange(len(obs_samples))
+    N = len(t_series)
     
     # ---- 参数估计 ---------------------------------------------------------------------------------
     
-    a_map, b_map, c_map, sigma_obs_map = estimate_params(t_series, obs_samples)
+    # a_map, b_map, c_map, sigma_obs_map = estimate_params(t_series, obs_samples)
     
     # ---- 基于粒子滤波的CI曲线估计 --------------------------------------------------------------------
     
-    A = ...
-    B = ...
+    """
+    这里通过粒子滤波而非参数估计获得所有的参数a、b、c、sigma_obs的估计值
+    """
     
-    obs_samples = obs_samples.reshape(1, -1)
-    N = obs_samples.shape[1]
+    n_marg_pts = 9  # 各边际上的颗粒数
+    p_ranges = [[-0.001, 0.002], [1300., 1500.], [-0.1, 0.1], [0.1, 10.]]  # 参数a、b、c、sigma_obs范围
     
-    n_marg_particles = 51           # 各边际上的颗粒数
-    sys_noise_std = 0.0             # 系统误差
-    obs_noise_std = sigma_obs_map   # 参数提醒: 参数估计
-    
-    x_range = [[-10, 100]]          # NOTE: 需要涵盖样本
-    
-    x_grids = np.meshgrid(
-        np.linspace(x_range[0][0], x_range[0][1], n_marg_particles)
+    p_grids = np.meshgrid(
+        *[np.linspace(p_range[0], p_range[1], n_marg_pts) for p_range in p_ranges]
         )
     
-    x_filtered = None
-    particles_lst = []
+    init_pts = np.c_[*[p_grids[i].flatten() for i in range(len(p_ranges))]]
     
-    for loc in range(N - 1):
-        print(f"loc = {loc}, \ttotal = {N - 1} \r", end="")
+    # 逐时间步迭代
+    pts_step_lst = []
+    
+    # TODO: 状态变量: a, x, dx, sigma
+    # 第0步：a, b, c, sigma
+    # 第t>=1步：a, x_t, dx_t, sigma
+    # 给出 t - 1 -> t 的迭代关系，进而计算权重，进而进行重采样
+    
+    for step in range(N):  # loc是当前位置时间步
+        print(f"step = {step}, \ttotal = {N} \r", end="")
         
         # 根据离散化网格初始化粒子集
-        if loc == 0:
-            particles = np.c_[x_grids[0].flatten()]
+        pts_rs: np.ndarray
         
-        particles_lst.append(particles)
+        if step == 0:
+            pts = init_pts
+        else:
+            pts = pts_rs
         
-        # 输出观测值
-        z_obs = obs_samples[:, loc + 1]
-        
-        # 前向并计算权重 
-        weights = np.array([])
-        
-        b = np.array([[1400]])
-        
-        for xi in particles:
-            xi = xi.reshape(2, 1)
-            sys_noise = np.random.normal(0, sys_noise_std)
-            obs_noise = np.random.normal(0, obs_noise_std)
+        # 记录此时间步上的粒子集
+        pts_step_lst.append(pts)
 
-            # 预测：前向计算
-            yi = np.dot(A, xi) + sys_noise.reshape(2, 1)
-            zi = np.dot(B, yi)
+        # 当前步粒子集
+        a_pts, x_pts, dx_pts, sigma_pts = pts[:, 0], pts[:, 1], pts[:, 2], pts[:, 3]
+        
+        # 计算该步的预测状态
+        if step == 0:
+            x_pred = x_pts
+            dx_pred = dx_pts
+        else:
+            # TODO: 改成前后两步状态迭代的格式
+            # t = t_series[step]
+            # x_pred = a_pts * t * (t - 1) / 2 + t * c_pts + b_pts
+            x_pred = ...
+        
+        # 观测值
+        x_obs = obs_samples[step]
+        
+        # 计算所有粒子的权重
+        w_pts = stats.norm.pdf(x_obs, x_pred, sigma_pts)
+        
+        # 重采样
+        pts_rs = resample(pts, w_pts)
+        
+        # break
+        
+        
+        
+        
+        
+    
+    # A = ...
+    # B = ...
+    
+    # obs_samples = obs_samples.reshape(1, -1)
+    # N = obs_samples.shape[1]
+    
+    # n_marg_pts = 51           # 各边际上的颗粒数
+    # sys_noise_std = 0.0             # 系统误差
+    # obs_noise_std = sigma_obs_map   # 参数提醒: 参数估计
+    
+    # x_range = [[-10, 100]]          # NOTE: 需要涵盖样本
+    
+    # x_grids = np.meshgrid(
+    #     np.linspace(x_range[0][0], x_range[0][1], n_marg_pts)
+    #     )
+    
+    # x_filtered = None
+    # pts_step_lst = []
+    
+    # for loc in range(N - 1):
+    #     print(f"loc = {loc}, \ttotal = {N - 1} \r", end="")
+        
+    #     # 根据离散化网格初始化粒子集
+    #     if loc == 0:
+    #         pts = np.c_[x_grids[0].flatten()]
+        
+    #     pts_step_lst.append(pts)
+        
+    #     # 输出观测值
+    #     z_obs = obs_samples[:, loc + 1]
+        
+    #     # 前向并计算权重 
+    #     weights = np.array([])
+        
+    #     b = np.array([[1400]])
+        
+    #     for xi in pts:
+    #         xi = xi.reshape(2, 1)
+    #         sys_noise = np.random.normal(0, sys_noise_std)
+    #         obs_noise = np.random.normal(0, obs_noise_std)
 
-            # 校正：根据输出与观测的匹配程度，计算各粒子的重要度权重 wi = p(y_obs|xi)
-            wi = stats.norm.pdf(z_obs.flatten(), zi.flatten(), obs_noise_std)[0]
-            weights = np.append(weights, wi)
+    #         # 预测：前向计算
+    #         yi = np.dot(A, xi) + sys_noise.reshape(2, 1)
+    #         zi = np.dot(B, yi)
+
+    #         # 校正：根据输出与观测的匹配程度，计算各粒子的重要度权重 wi = p(y_obs|xi)
+    #         wi = stats.norm.pdf(z_obs.flatten(), zi.flatten(), obs_noise_std)[0]
+    #         weights = np.append(weights, wi)
             
-        # 重要性采样
-        particles_resampled = resample(particles, weights)
-        assert particles_resampled is not None
+    #     # 重要性采样
+    #     pts_resampled = resample(pts, weights)
+    #     assert pts_resampled is not None
         
-        # 记录本环节输入状态值期望 E[x_sys]
-        a = np.mean(particles_resampled, axis=0).reshape(2, 1)
-        x_filtered = a if x_filtered is None else np.hstack((x_filtered, a))
+    #     # 记录本环节输入状态值期望 E[x_sys]
+    #     a = np.mean(pts_resampled, axis=0).reshape(2, 1)
+    #     x_filtered = a if x_filtered is None else np.hstack((x_filtered, a))
         
-        # 更新粒子: 预测下一时刻的N个粒子
-        sys_noise = np.vstack((
-            np.random.normal(0, sys_noise_std[0], n_marg_particles ** 2),
-            np.random.normal(0, sys_noise_std[1], n_marg_particles ** 2)
-        ))
-        particles = np.dot(A, particles_resampled.T).T + sys_noise.T
+    #     # 更新粒子: 预测下一时刻的N个粒子
+    #     sys_noise = np.vstack((
+    #         np.random.normal(0, sys_noise_std[0], n_marg_pts ** 2),
+    #         np.random.normal(0, sys_noise_std[1], n_marg_pts ** 2)
+    #     ))
+    #     pts = np.dot(A, pts_resampled.T).T + sys_noise.T
